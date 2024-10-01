5ae1fc75dea56a54d79160ac5a6632cc2d93c7b0
Preet Taparia <preettaparia@gmail.com>
2024-10-01 12:41:30 +0530

    a little better ir reading

--- pages/api/ir-data.js
+++ pages/api/ir-data.js
@@ -1,9 +1,13 @@
 export default async function handler(req, res) {
     if (req.method === 'POST') {
-      const { ir_code, device } = req.body;
-        res.status(200).json({ message: 'IR data received successfully' });
+      const { ir_code } = req.body;
+  
+      if (!ir_code) {
+        return res.status(400).json({ message: 'IR code is missing' });
+      }  
+      return res.status(200).json({ message: 'IR data received successfully', ir_code });
     } else {
-      res.status(405).json({ message: 'Only POST requests are allowed' });
+      return res.status(405).json({ message: 'Method Not Allowed. Use POST.' });
     }
   }
   
